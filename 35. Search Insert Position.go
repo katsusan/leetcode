c2b698d134eb5cef0b22950406e796f352dbbbdd
Katsu <evergonuaa@gmail.com>
2018-12-22 15:20:48 +0800

    leetcode-35 optm

--- 35. Search Insert Position.go
+++ 35. Search Insert Position.go
@@ -3,27 +3,18 @@ package main
 func searchInsert(nums []int, target int) int {
 	//use binary search, O(n) = lg(n)
 
-	if target > nums[len(nums)-1] {
-		return len(nums)
-	} else if target < nums[0] {
-		return 0
-	}
-
-	for i, j := 0, len(nums); ; {
-
-		if j == (i+1) && nums[i] < target && nums[j] > target {
-			return j
-		}
+	i, j := 0, len(nums)-1
+	for i <= j {
 
 		if target < nums[(i+j)/2] {
-			j = (i + j) / 2
+			j = (i+j)/2 - 1
 			continue
 		} else if target > nums[(i+j)/2] {
-			i = (i + j) / 2
+			i = (i+j)/2 + 1
 			continue
 		} else {
 			return (i + j) / 2
 		}
-
 	}
+	return i
 }
