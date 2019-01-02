258aededf27b9252e4800abbea45092522a0ad40
Katsu <evergonuaa@gmail.com>
2019-01-02 21:55:38 +0800

    leetcode-167

---
+++ 167. Two Sum II - Input array is sorted.go
@@ -0,0 +1,19 @@
+package main
+
+func twoSum_167(numbers []int, target int) []int {
+	i := 0
+	j := len(numbers) - 1
+	for i != j {
+		sum := numbers[i] + numbers[j]
+		if sum == target {
+			return []int{i + 1, j + 1}
+		} else if sum > target {
+			j--
+			continue
+		} else if sum < target {
+			i++
+			continue
+		}
+	}
+	return []int{}
+}
