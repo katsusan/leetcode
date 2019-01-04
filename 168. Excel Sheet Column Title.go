ca0152568ecebf009f07aafda8127b6e60850d39
Katsu <evergonuaa@gmail.com>
2019-01-04 15:46:23 +0800

    leetcode-168/169

---
+++ 168. Excel Sheet Column Title.go
@@ -0,0 +1,12 @@
+package main
+
+func convertToTitle(n int) string {
+	if n <= 26 {
+		return string('A' + n - 1)
+	}
+	rmd := n % 26
+	if rmd == 0 {
+		return convertToTitle((n-26)/26) + "Z"
+	}
+	return convertToTitle(n/26) + string('A'+rmd-1)
+}
