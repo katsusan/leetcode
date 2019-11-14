22520f79178c8430ba14dddcc8c8838703037081
Katsu <evergonuaa@gmail.com>
2019-11-14 13:32:42 +0800

    update to using hash

--- 1. Two Sum.go
+++ 1. Two Sum.go
@@ -1,17 +1,19 @@
 package main
 
-//my solution ->70ms level
+//my solution
 func twoSum(nums []int, target int) []int {
-	var i, j int
-	var result []int
-	for i = 0; i < len(nums); i++ {
-		for j = i + 1; j < len(nums); j++ {
-			if nums[i]+nums[j] == target {
-				result = []int{i, j}
-			}
+	m := make(map[int]int)
+
+	for i := range nums {
+		m[nums[i]] = i
+	}
+
+	for j := range nums {
+		if v, ok := m[target-nums[j]]; ok && j != v {
+			return []int{j, v}
 		}
 	}
-	return result
+	return []int{}
 }
 
 //better solution ->4ms level
