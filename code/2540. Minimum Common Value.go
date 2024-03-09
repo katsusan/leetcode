34191eadff76d061a7c2169427bd74f2beb35036
Katsu <evergonuaa@gmail.com>
2024-03-09 14:06:13 +0800

    leetcode-2540-two pointers

---
+++ code/2540. Minimum Common Value.go
@@ -0,0 +1,18 @@
+package code
+
+func getCommon(nums1 []int, nums2 []int) int {
+	i, j := 0, 0
+	for i < len(nums1) && j < len(nums2) {
+		if nums1[i] == nums2[j] {
+			return nums1[i]
+		}
+
+		if nums1[i] > nums2[j] {
+			j++
+		} else {
+			i++
+		}
+	}
+
+	return -1
+}
